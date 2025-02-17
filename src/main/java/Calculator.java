d493c13f01a6004cd52aa16d54853df080ee5034
phodzo nagana <phodzonagz12@gmail.com>
2025-02-17 10:53:46 +0200

    added calculator and unit test

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,4 +1,38 @@
-package PACKAGE_NAME;
-
 public class Calculator {
+    public int add(int...numbers){
+        int sum=0;
+        for(int num : numbers){
+            sum +=num;
+        }
+        return sum;
+    }
+    public int multiply(int...numbers){
+        int product =1;
+        for (int num: numbers){
+            product *=num;
+        }
+        return product;
+    }
+    public int division(int... numbers) {
+        if (numbers.length == 0) return 0;
+
+        int divided = numbers[0];
+        for(int i = 1; i < numbers.length; i++){
+            if(numbers[i] == 0) throw new ArithmeticException("Number cannot divide by zero!");
+            divided /= numbers[i];
+        }
+        return divided;
+    }
+
+    public int subtraction(int...numbers) {
+        if (numbers.length == 0)
+            return 0;
+        {
+            int subtracted = numbers[0];
+            for (int i = 1; i < numbers.length; i++) {
+                subtracted -= numbers[i];
+            }
+            return subtracted;
+        }
+    }
 }
--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,4 +1,34 @@
-package PACKAGE_NAME;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 public class CalculatorTest {
+    @Test
+    public void testTwoIntegers(){
+        Calculator calculator = new Calculator();
+        assertEquals(15,calculator.add(1,2,3,4,5));
+        assertEquals(5, calculator.add(1,2));
+        assertEquals(-4,calculator.add(-1,-1));
+    }
+    @Test
+    public void testMultiplyTwoIntegers(){
+        Calculator calculator = new Calculator();
+        assertEquals(4,calculator.multiply(1,3));
+        assertEquals(-3,calculator.multiply(-1,3));
+        assertEquals(120,calculator.multiply(1, 2, 3, 4, 5));
+    }
+    @Test
+    public void testDivisionToIntegers() {
+        Calculator calculator = new Calculator();
+        assertEquals(2, calculator.division(10, 5));
+        assertEquals(20, calculator.division(40, 2));
+        assertEquals(0, calculator.division(5, 10));
+    }
+    @Test
+    public void testSubtractionTwoIntegers(){
+        Calculator calculator = new Calculator();
+        assertEquals(12,calculator.subtraction(14,2));
+        assertEquals(5,calculator.subtraction(10,5));
+        assertEquals(100,calculator.subtraction(130,30));
+    }
 }
